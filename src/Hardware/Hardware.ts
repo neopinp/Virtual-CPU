ba419ea610fd1cb6712c0fd0f273620323262da6
neopinp <123033639+neopinp@users.noreply.github.com>
2024-04-06 19:22:22 -0400

    crct

--- src/Hardware/Hardware.ts
+++ src/Hardware/Hardware.ts
@@ -20,9 +20,16 @@ export class Hardware {
     }
   }
 
-// logs hex value 
-  public hexLog(value: number, length: number = 2): void {
-    const hexValue = value.toString(16).toUpperCase().padStart(length, '0');
-    console.log(`[HW - ${this.name} id: ${this.id}]: ${hexValue}`);
+
+public hexLog(address: number, data?: number, addressLength: number = 4, dataLength: number = 2): void {
+  const hexAddress = address.toString(16).toUpperCase().padStart(addressLength, '0');
+  let message = `Address: ${hexAddress}`;
+  if (data !== undefined) {
+      const hexData = data.toString(16).toUpperCase().padStart(dataLength, '0');
+      message += ` Contains Value: ${hexData}`;
   }
+  if (this.debug) {
+      console.log(`[HW - ${this.name} id: ${this.id}]: ${message}`);
+  }
+}
 }
